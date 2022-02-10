125b9d1a4d6c858eccb859815e3cfc0d96a2bed4
AliAli112 <alioren112@outlook.com>
2022-02-10 14:16:37 -0600

    css pt1

--- src/app/shared/saved-notes/saved-notes.component.ts
+++ src/app/shared/saved-notes/saved-notes.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { Note } from '../../misc/Note';
 
 @Component({
@@ -8,15 +8,22 @@ import { Note } from '../../misc/Note';
 })
 export class SavedNotesComponent implements OnInit {
   @Input() savedNote: Note;
-
+  backgrounds: string[] = ['../../../assets/image/nightsky.png',
+    '../../../assets/image/nightsky2.png','../../../assets/image/nightsky3.png'];
+  randomInt = 0;
   constructor() {
-
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    //this.randomInt = Math.floor(Math.random() * this.backgrounds.length);
+  }
 
   public isNoteModified(): boolean{
     return this.savedNote.dateModified !== undefined;
-    console.log(this.savedNote.dateModified);
+  }
+
+  public randomBackground(){
+    // console.log(this.backgrounds[this.randomInt]);
+    // return this.backgrounds[this.randomInt];
   }
 }
