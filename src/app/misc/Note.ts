3bdbb90eccd30c7bb914de244ddbad3401301a52
AliAli112 <alioren112@outlook.com>
2022-02-11 11:16:05 -0600

    pre cap

--- src/app/misc/Note.ts
+++ src/app/misc/Note.ts
@@ -4,7 +4,6 @@ export class Note {
   public dateModified: Date;
   public title: string;
   public body: string;
-  public background: string;
   constructor(
     title: string,
     body: string
@@ -15,10 +14,6 @@ export class Note {
     this.id = title + '_' + d.valueOf().toString();
     this.title = title;
     this.body = body;
-    const backgrounds: string[] = ['../../../assets/image/nightsky.png',
-    '../../../assets/image/nightsky2.png','../../../assets/image/nightsky3.png'];
-    this.background = backgrounds[Math.floor(Math.random() * backgrounds.length)];
-    console.log(this.background);
   }
 
 }
--- src/app/shared/saved-notes/saved-notes.component.ts
+++ src/app/shared/saved-notes/saved-notes.component.ts
@@ -8,22 +8,16 @@ import { Note } from '../../misc/Note';
 })
 export class SavedNotesComponent implements OnInit {
   @Input() savedNote: Note;
-  // backgrounds: string[] = ['../../../assets/image/nightsky.png',
-  //   '../../../assets/image/nightsky2.png','../../../assets/image/nightsky3.png'];
   randomInt = 0;
   constructor() {
   }
 
   ngOnInit() {
-    //this.randomInt = Math.floor(Math.random() * this.backgrounds.length);
+
   }
 
   public isNoteModified(): boolean{
     return this.savedNote.dateModified !== undefined;
   }
 
-  public randomBackground(){
-    // console.log(this.backgrounds[this.randomInt]);
-    // return this.backgrounds[this.randomInt];
-  }
 }
